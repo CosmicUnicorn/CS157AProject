18e6b2d7874c8d7cb2e3cf042790ef42c229d49b
Jamie Taylor <james.taylor@sjsu.edu>
2021-10-20 11:01:47 -0700

    Test commit

--- src/main/Console.java
+++ src/main/Console.java
@@ -1,5 +1,27 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Console {
 
+	public static void main(String[] args)
+	{
+		Connection connection = null;
+
+        try {
+               connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventoryDB?serverTimezone=UTC","root", "password");
+        } catch (SQLException e) {
+                System.out.println("Connection Failed...");
+                e.printStackTrace();
+                return;
+        }
+
+        if (connection != null) {
+                System.out.println("Connection Succeeded.");
+        } else {
+                System.out.println("Failed to make connection.");
+        }
+	}
 }
