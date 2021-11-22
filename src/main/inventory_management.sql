9c06b3b4e8cceed12c426f4214675e776c788797
Jamie Taylor <james.taylor@sjsu.edu>
2021-11-22 09:35:03 -0800

    Fixed foreign keys

--- src/main/inventory_management.sql
+++ src/main/inventory_management.sql
@@ -6,15 +6,6 @@ drop table if exists Suppliers;
 drop table if exists Transactions;
 drop table if exists ArchivedOrders;
 
-create table Orders (
-    customerID int not null references Customers(id) on delete cascade,
-    transactionID int not null primary key references Transactions(id),
-    productID int references Products(id),
-    quantity int,
-    status text,
-    updatedAt datetime default current_timestamp
-);
-
 create table Admin (
     id int auto_increment primary key,
     name text not null,
@@ -29,36 +20,56 @@ create table Customers (
     address text not null
 );
 
+create table Suppliers (
+    id int auto_increment primary key,
+    name text not null
+);
+
 create table Products (
     id int auto_increment primary key,
     title text not null,
     cost double not null,
     quantity int,
     weight double,
-    supplierID int references Suppliers(id) on delete set null
+    supplierID int,
+    FOREIGN KEY (supplierID) REFERENCES Suppliers(id)
 );
 
-create table Suppliers (
-    id int auto_increment primary key,
-    name text not null
-);
+
 
 create table Transactions (
     id int auto_increment primary key,
-    customerID int references Customers(id) on delete cascade,
-    productID int references Products(id),
+    customerID int,
+    productID int,
     quantity int,
     totalAmount double default 0.0,
-    transactionDate datetime default current_timestamp
+    transactionDate datetime default current_timestamp,
+    FOREIGN KEY (customerID) REFERENCES Customers(id) on delete cascade,
+    FOREIGN KEY (productID) REFERENCES Products(id)
+);
+
+create table Orders (
+    customerID int not null,
+    transactionID int not null primary key,
+    productID int,
+    quantity int,
+    status text,
+    updatedAt datetime default current_timestamp,
+    FOREIGN KEY (customerID) REFERENCES Customers(id) on delete cascade,
+    FOREIGN KEY (productID) REFERENCES Products(id),
+    FOREIGN KEY (transactionID) REFERENCES Transactions(id)
 );
 
 create table ArchivedOrders (
-    customerID int references Customers(id) on delete cascade,
-    transactionID int primary key references Transactions(id),
-    productID int references Products(id),
+    customerID int,
+    transactionID int primary key,
+    productID int,
     quantity int,
     status text,
-    updatedAt datetime
+    updatedAt datetime,
+    FOREIGN KEY (customerID) REFERENCES Customers(id) on delete cascade,
+    FOREIGN KEY (productID) REFERENCES Products(id),
+    FOREIGN KEY (transactionID) REFERENCES Transactions(id)
 );
 
 
